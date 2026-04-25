5b09f3eed0b3d6bca39de27b96830d4860f55c57
dev-salih <salihmu79@gmail.com>
2026-04-25 12:12:37 +0300

    password generating is done

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const passwordBox = document.getElementById("password");
 const length = 12;
+const button = document.getElementById("btn");
 
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
 const symbol = "@#$%^&*()_+|}{[]></-=";
+
+const allChars = upperCase + lowerCase + number + symbol;
 
 function createPassword() {
     let password = "";
@@ -12,5 +15,13 @@ function createPassword() {
     password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
     password += number[Math.floor(Math.random() * number.length)];
     password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
 }
+
+button.addEventListener("click", createPassword);
+
 
